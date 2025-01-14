d38de283c51f90aee61500b0f2d8e317e8801076
Eshed Ortal <eshed.ortal@outlook.com>
2025-01-14 12:13:29 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask App running with CI/CD!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
